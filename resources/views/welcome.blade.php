d745778d271f332c2b0b9ed780841cd91639d287
ejay010 <ejay010@gmail.com>
2016-11-15 14:16:05 -0500

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Eulond Kelly III</title>
+
+        <!-- Fonts -->
+
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="/css/materialize.min.css">
+
+        <link rel="stylesheet" href="/css/animate.css">
+
+        <link href='//fonts.googleapis.com/css?family=Roboto:400,100,300,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+        <script src="https://use.fontawesome.com/2299fe1e0e.js"></script>
+
+        <link rel="stylesheet" href="/css/resumeStyle.css">
+
+        <link rel="stylesheet" href="/css/responsive.css">
+
+
+        <link rel="stylesheet" href="/css/resumeColors.css">
+
+        <!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif] -->
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+    <body>
+    <!-- Start Container-->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <!-- =========================================
+                           SIDEBAR
+            ==========================================-->
+            <!-- Start Sidebar -->
+            <aside class="col l4 m12 s12 sidebar z-depth-1" id="sidebar">
+                <!--  Sidebar row -->
+                <div class="row">
+                    <!--  top section   -->
+                    <div class="heading">
+                        <!-- ====================
+                                  IMAGE
+                        ==========================-->
+                        <div class="feature-img">
+                            <a href="index.html"><img src="http://i1237.photobucket.com/albums/ff463/ejay0101/37243_454463525827_348571_n_zpsiokwuqqe.jpg" class="responsive-img" alt=""></a>
+                        </div>
+                        <!-- =========================================
+                                   NAVIGATION
+                        ==========================================-->
+                        {{--<div class=" nav-icon">--}}
+                            {{--<nav>--}}
+                                {{--<div class="nav-wrapper">--}}
+                                    {{--<ul id="nav-mobile" class="side-nav">--}}
+                                        {{--<li><a href="index.html">Resume</a></li>--}}
+                                        {{--<li><a href="project.html">Projects</a></li>--}}
+                                        {{--<li><a href="cover-latter.html">Cover Latter</a></li>--}}
+                                        {{--<li><a href="blog.html">Blog</a></li>--}}
+                                        {{--<li><a href="contact.html">Contact</a></li>--}}
+                                    {{--</ul>--}}
+                                    {{--<a href="#" data-activates="nav-mobile" class="button-collapse  "><i class="mdi-navigation-menu"></i></a>--}}
+                                {{--</div>--}}
+                            {{--</nav>--}}
+                        {{--</div>--}}
+                        <!-- ========================================
+                                   NAME AND TAGLINE
+                        ==========================================-->
+                        <div class="title col s12 m12 l9 right  wow fadeIn" data-wow-delay="0.1s">
+                            <h2>Eulond Kelly III</h2> <!-- title name -->
+                            <span>Computer Information Science</span>  <!-- tagline -->
+                        </div>
+                    </div>
+                    <!-- sidebar info -->
+                    <div class="col l12 m12 s12 sort-info sidebar-item">
+                        <div class="row">
+                            <div class="col m12 s12 l3 icon"> <!-- icon -->
+                                <i class="fa fa-user"></i>
+                            </div>
+                            <div class="col m12 s12 l9 info wow fadeIn a1" data-wow-delay="0.1s" > <!-- text -->
+                                <div class="section-item-details">
+                                    <p>I desire to be employed within an impactful organization, that is interested in using technology to
+                                    bring about positive change to the community around it and present me with new and exciting challenges that
+                                    can help me to grow, mentally and personally, as I help them to prosper.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- MOBILE NUMBER -->
+                    <div class="col l12 m12 s12  mobile sidebar-item">
+                        <div class="row">
+                            <div class="col m12 s12 l3 icon">
+                                <i class="fa fa-phone"></i> <!-- icon -->
+                            </div>
+                            <div class="col m12 s12 l9 info wow fadeIn a2" data-wow-delay="0.2s" >
+                                <div class="section-item-details">
+                                    <div class="personal">
+                                        <h4><a href="tel:786-853-1646">(786)-853-1646</a></h4> <!-- Number -->
+                                        <span>mobile</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--  EMAIL -->
+                    <div class="col l12 m12 s12  email sidebar-item ">
+                        <div class="row">
+                            <div class="col m12 s12 l3 icon">
+                                <i class="fa fa-envelope"></i> <!-- icon -->
+                            </div>
+                            <div class="col m12 s12 l9 info wow fadeIn a3" data-wow-delay="0.3s">
+                                <div class="section-item-details">
+                                    <div class="personal">
+                                        <h4><a href="mailto:eulond.kelly@mymail.barry.edu">eulond.kelly@mymail.barry.edu</a></h4> <!-- Email -->
+                                        <span>personal</span>
+                                    </div>
+                                    <div class="work">
+                                        <h4><a href="mailto:ejay010@gmail.com">ejay010@gmail.com</a></h4> <!-- Email -->
+                                        <span>work</span> <br>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- SKILLS -->
+                    <div class="col l12 m12 s12  skills sidebar-item" >
+                        <div class="row">
+                            <div class="col m12 l3 s12 icon">
+                                <i class="fa fa-calendar-o"></i> <!-- icon -->
+                            </div>
+                            <!-- Skills -->
+                            <div class="col m12 l9 s12 skill-line a5 wow fadeIn" data-wow-delay="0.5s">
+                                <h3>Professional Skills </h3>
+
+                                <span>Adobe Photoshop</span>
+                                <div class="progress">
+                                    <div class="determinate"> 85% </div>
+                                </div>
+
+                                <span>HTML</span>
+                                <div class="progress">
+                                    <div class="determinate"> 95%</div>
+                                </div>
+
+                                <span>CSS</span>
+                                <div class="progress">
+                                    <div class="determinate">87%</div>
+                                </div>
+
+                                <span>Javascript</span>
+                                <div class="progress">
+                                    <div class="determinate">88%</div>
+                                </div>
+
+                                <span>PHP</span>
+                                <div class="progress">
+                                    <div class="determinate">90%</div>
+                                </div>
+
+                                <span>JAVA</span>
+                                <div class="progress">
+                                    <div class="determinate">85%</div>
+                                </div>
+
+                                <span>SQL</span>
+                                <div class="progress">
+                                    <div class="determinate">70%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>   <!-- end row -->
+            </aside><!-- end sidebar -->
+
+            <!-- =========================================
+               Work experiences
+            ==========================================-->
+
+            <section class="col s12 m12 l8 section">
+                <div class="row">
+                    <div class="section-wrapper z-depth-1">
+                        <div class="section-icon col s12 m12 l2">
+                            <i class="fa fa-suitcase"></i>
+                        </div>
+                        <div class="custom-content col s12 m12 l10 wow fadeIn a1" data-wow-delay="0.1s">
+                            <h2>Work Experience</h2>
+
+                            <div class="custom-content-wrapper wow fadeIn a2" data-wow-delay="0.2s">
+                                <h3>Computer Technician <span>@South Florida Digital Alliance</span></h3>
+                                <span>AUG 2016 - DEC 2016 </span>
+                                <ul>
+                                    <li>Semester Internship</li>
+                                    <li>Refurbishing Computers</li>
+                                    <li>Repairing Computers</li>
+                                    <li>Cloning computer operating system</li>
+                                </ul>
+                            </div>
+                            <div class="custom-content-wrapper wow fadeIn a3" data-wow-delay="0.3s">
+                                <h3>Summer Internship <span>@The Consulate General of The Bahamas, Miami </span></h3>
+                                <span>MAY 2016 - JUL 2016 </span>
+                                <ul>
+                                    <li>Telephone Customer Service</li>
+                                    <li>Designed Flyers for Consulate Events</li>
+                                    <li>Designed Souviner Booklet for Independence celebration</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- ========================================
+                     Education
+                    ==========================================-->
+
+                    <div class="section-wrapper z-depth-1">
+                        <div class="section-icon col s12 m12 l2">
+                            <i class="fa fa-graduation-cap"></i>
+                        </div>
+                        <div class="custom-content col s12 m12 l10 wow fadeIn a1" data-wow-delay="0.1s" >
+                            <h2>Education </h2>
+
+                            <div class="custom-content-wrapper wow fadeIn a2" data-wow-delay="0.2s" >
+                                <h3>Computer Information Science Bachelor of Arts & Science<span>@Barry University</span></h3>
+                                <span>Aug 2012 - DEC 2016 </span>
+                                <p>With a minor in Business Management </p>
+                                <p>Extracurricular activities:</p>
+                                <ul>
+                                    <li>Caribbean Student Association, General Member 2012 – Present</li>
+                                    <li>Bahamian Student Association, General Member 2014 – Present</li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <!-- ========================================
+                          Intertests
+                    ==========================================-->
+
+                    {{--<div class="section-wrapper z-depth-1">--}}
+                        {{--<div class="section-icon col s12 m12 l2">--}}
+                            {{--<i class="fa fa-plane"></i>--}}
+                        {{--</div>--}}
+                        {{--<div class="interests col s12 m12 l10 wow fadeIn" data-wow-delay="0.1s">--}}
+                            {{--<h2>Interestes </h2>--}}
+                            {{--<ul> <!-- interetsr icon start -->--}}
+                                {{--<li><i class="fa fa-camera-retro tooltipped" data-position="top" data-delay="50" data-tooltip="Photography"></i></li>--}}
+                                {{--<li><i class="fa fa-glass tooltipped" data-position="top" data-delay="50" data-tooltip="Drinking"></i></li>--}}
+                                {{--<li><i class="fa fa-headphones tooltipped" data-position="top" data-delay="50" data-tooltip="Music"></i></li>--}}
+                                {{--<li><i class="fa fa-comments tooltipped" data-position="top" data-delay="50" data-tooltip="Chatting"></i></li>--}}
+                                {{--<li><i class="fa fa-coffee tooltipped" data-position="top" data-delay="50" data-tooltip="Coffee"></i></li>--}}
+                            {{--</ul> <!-- interetsr icon end -->--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    <!-- =======================================
+                      portfolio Website
+                    ==========================================-->
+
+                    <div class="section-wrapper z-depth-1 wow fadeIn" data-wow-delay="0.1s">
+                        <div class="col s12 m12 l10 website right" >
+                            <div class="row">
+                                <div class="col s12 m12 l6">
+                                    <p><span>{{Html::link('www.github.com/ejay010', 'Checkout my github')}}</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- end row -->
+            </section><!-- end section -->
+        </div> <!-- end row -->
+    </div>  <!-- end container -->
+
+    <!--=====================
+                   JavaScript
+           ===================== -->
+    <!-- Jquery core js-->
+    <script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
+    <!-- materialize js-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+
+    <!-- wow js-->
+    <script src="/js/wow.min.js"></script>
+
+    <!-- Masonry js-->
+    <script src="/js/masonry.js"></script>
+
+    <script src="/js/validator.js"></script>
+
+    <!-- Customized js -->
+    <script src="/js/cus.js"></script>
+
+    </body>
+</html>
